e051fc27377d6e0a6cfc6f31247879d2d6cd3f6c
Emillyrayssa654 <127302973+Emillyrayssa654@users.noreply.github.com>
2023-11-22 08:51:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
    function tocaSom(idElementoAudio) {
       document.querySelector(idElementoAudio).play();
    }
- 
+
   const listaDeteclas = document.querySelectorAll('.tecla');
 
   let contador =0
 
   //enquanto
-  while(contador < listaDeteclas.length) 
-  {
+  while(contador < listaDeteclas.length) {
 
-    listaDeteclas[contador].onclick = tocaSom;
+   const tecla=listaDeteclas[contador]
+   const instrumento =tecla.classList[1];
+
+   console.log(instrumento)
+
+   const idAudio = `#som_{instrumento}`
+
+   //'#som_(instrumento)'
+
+    tecla.onclick = function() {
+      tocaSom('#som_tecla_som');
+    }
 
     contador=contador+1;
 
